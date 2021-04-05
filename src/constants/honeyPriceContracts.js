6db9a33f43d2666f0f4b5e7ead82b12bd3df0758
Mihir <mwadekar2000@gmail.com>
2021-04-04 19:08:57 -0700

    Revamped and added new LP addresses

--- src/constants/honeyPriceContracts.js
+++ src/constants/honeyPriceContracts.js
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
+  cakeAddress: '0x1BD7dF20aE5e884C61b7017FaC557e82cCA6baEe',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
+  lpAddress: '0x64510b70aad51418bd17b50226bd08cbec48bddf'
 }
 
 export default priceContracts
